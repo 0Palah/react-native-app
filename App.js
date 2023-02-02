e7ee885c6f707b7def76e6c9f0f20110e764ca6a
Oleg Palashynskyi <106759871+0Palah@users.noreply.github.com>
2023-02-02 21:49:02 +0200

    BTN

--- App.js
+++ App.js
@@ -6,11 +6,16 @@ import {
   View,
   TextInput,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
 
 export default function App() {
-  const [value, setValue] = useState("");
-  const inputHandler = (text) => setValue(text);
+  const [loginValue, setLoginValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+  const inputHandlerLogin = (text) => setLoginValue(text);
+  const inputHandlerEmail = (text) => setEmailValue(text);
+  const inputHandlerPassword = (text) => setPasswordValue(text);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -22,14 +27,40 @@ export default function App() {
           <View style={styles.titleWrapper}>
             <Text style={styles.title}>Регистрация</Text>
           </View>
-          <View style={styles.test}>
+          <View style={styles.inpupWrapperLoginEmail}>
             <TextInput
-              style={styles.inputLogin}
+              style={styles.inputLoginEmail}
               placeholder="Логин"
-              value={value}
-              onChangeText={inputHandler}
+              placeholderTextColor="#BDBDBD"
+              value={loginValue}
+              onChangeText={inputHandlerLogin}
             />
           </View>
+          <View style={styles.inpupWrapperLoginEmail}>
+            <TextInput
+              style={styles.inputLoginEmail}
+              placeholder="Адрес электронной почты"
+              placeholderTextColor="#BDBDBD"
+              value={emailValue}
+              onChangeText={inputHandlerEmail}
+            />
+          </View>
+          <View style={styles.inpupWrapperPassword}>
+            <TextInput
+              style={styles.inputPassword}
+              placeholder="Пароль"
+              placeholderTextColor="#BDBDBD"
+              value={passwordValue}
+              onChangeText={inputHandlerPassword}
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            // onPress={onPress}
+          >
+            <Text style={styles.btnName}>Зарегистрироваться</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
@@ -48,16 +79,25 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "cover",
     justifyContent: "flex-end",
-
     // alignItems: "center",
+  },
+
+  formWrapper: {
+    height: 550,
+    paddingHorizontal: 16,
+    paddingTop: 92,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#fff",
   },
 
   titleWrapper: {
     alignItems: "center",
+    marginBottom: 32,
   },
 
   title: {
-    marginTop: 92,
+    // marginTop: 92,
     color: `#212121`,
     fontSize: 30,
     letterSpacing: 0.01,
@@ -67,38 +107,43 @@ const styles = StyleSheet.create({
     // font-style: normal;
   },
 
-  formWrapper: {
-    height: 550,
-    borderWidth: 1,
-    borderColor: "red",
-    paddingHorizontal: 16,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    backgroundColor: "#fff",
+  inpupWrapperLoginEmail: {
+    marginBottom: 16,
   },
 
-  inputLogin: {
+  inputLoginEmail: {
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 8,
     backgroundColor: "#F6F6F6",
     height: 50,
     paddingHorizontal: 16,
-    marginTop: 32,
+    // placeholderTextColor: "#BDBDBD",
   },
-  // test: {
-  //   flex: 1,
-  //   backgroundColor: "transparent",
-  //   alignItems: "center",
-  //   justifyContent: "flex-end",
-  //   paddingBottom: 30,
-  // },
 
-  imgBG: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "flex-end",
+  inpupWrapperPassword: {
+    marginBottom: 43,
+  },
 
-    // alignItems: "center",
+  inputPassword: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    paddingHorizontal: 16,
+  },
+
+  button: {
+    backgroundColor: "#FF6C00",
+    height: 51,
+    borderRadius: 100,
+    paddingVertical: 16,
+  },
+
+  btnName: {
+    fontSize: 16,
+    color: "#ffffff",
+    textAlign: "center",
   },
 });
